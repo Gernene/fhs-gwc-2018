36ece7ebc077322d3bed8e5f04307d4884ff89e4
Gernene <wallie988@gmail.com>
2018-02-08 16:38:30 -0700

    Start Can1 animation

---
+++ Assets/Scripts/Can1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Can1Controller : MonoBehaviour {
+
+	public Animation Can1;
+	public Animator Can1Animator;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("We are in start!");
+		Can1Animator.Play("Can1Animation");
+		Can1.clip.isLooping = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
